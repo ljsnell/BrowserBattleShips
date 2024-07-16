93b9b6e0302914a5a17bb51cd823de85f5ffe184
ljsnell <ljsnell7@gmail.com>
2024-07-15 20:56:08 -0500

    Not thrilled with the formatting but it's decent.

--- browser-battle-ships/src/App.js
+++ browser-battle-ships/src/App.js
@@ -2,15 +2,19 @@
 // Filename - App.js
 import './App.css';
 import BattleShip from './BattleShip';
+import React from 'react';
+import {
+    Button
+} from 'react-native';
 
 const data = [
-    { range: "Extreme", occupant: ""},
-    { range: "Long", occupant: ""},
-    { range: "Medium-Long", occupant: <BattleShip name="Ol' Wheez" country="🏴"/>},
-    { range: "Medium", occupant: ""},
-    { range: "Short", occupant: ""},
+    { range: "Extreme", occupant: "" },
+    { range: "Long", occupant: "" },
+    { range: "Medium-Long", occupant: <BattleShip name="Ol' Wheez" country="🏴" /> },
+    { range: "Medium", occupant: "" },
+    { range: "Short", occupant: "" },
 ]
- 
+
 function App() {
     return (
         <div className="App">
@@ -27,8 +31,25 @@ function App() {
                         </tr>
                     )
                 })}
+                <tr>
+                    <Button
+                        title="Close Range"
+                        color="blue"                        
+                    />
+                    <br></br>
+                    <Button
+                        title="Offer Broadside"
+                        color="blue"
+                    />
+                    <br></br>
+                    <Button                        
+                        title="Withdraw"                    
+                        color="blue"
+                    />
+                </tr>
             </table>
         </div>
+
     );
 }
 
