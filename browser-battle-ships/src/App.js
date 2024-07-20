bf310789f385db846a649cfb653cbc0cc73e179d
ljsnell <ljsnell7@gmail.com>
2024-07-20 17:35:52 -0500

    Ship moves a little bit.

--- browser-battle-ships/src/App.js
+++ browser-battle-ships/src/App.js
@@ -2,21 +2,22 @@
 // Filename - App.js
 import './App.css';
 import BattleShip from './BattleShip';
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Button
 } from 'react-native';
 import { pickAction } from './ShipAI';
 
-const data = [
-    { range: "Extreme", occupant: "" },
-    { range: "Long", occupant: "" },
-    { range: "Medium-Long", occupant: <BattleShip name="Ol' Wheez" country="🏴" previousAction="" /> },
-    { range: "Medium", occupant: "" },
-    { range: "Short", occupant: "" },
-]
+const emptyOcean = [{ range: "Extreme", occupant: "" },
+{ range: "Long", occupant: "" },
+{ range: "Medium-Long", occupant: "" },
+{ range: "Medium", occupant: "" },
+{ range: "Short", occupant: "" }]
 
 function App() {
+    const [shipIndex, setShipIndex] = useState(2)
+    const [shipData, setShipData] = useState(<BattleShip name="Ol' Wheez" country="🏴" previousAction="" />)
+    const [oceanData, setOceanData] = useState(emptyOcean)
     return (
         <div className="App">
             <table>
@@ -24,7 +25,7 @@ function App() {
                     <th>Range</th>
                     <th>Enemy Ships</th>
                 </tr>
-                {data.map((val, key) => {
+                {oceanData.map((val, key) => {
                     return (
                         <tr key={key}>
                             <td>{val.range}</td>
@@ -35,24 +36,32 @@ function App() {
                 <tr>
                     <Button
                         title="Close Range"
-                        color="blue"                        
+                        color="blue"
+                        onPress={() => moveShip(1, shipIndex, shipData, setShipIndex)}
                     />
                     <br></br>
                     <Button
                         title="Offer Broadside"
                         color="blue"
+                        onPress={() => moveShip(0)}
                     />
                     <br></br>
-                    <Button                        
-                        title="Withdraw"                    
+                    <Button
+                        title="Withdraw"
                         color="blue"
-                        onPress={pickAction}
+                        onPress={() => moveShip(-1)}
                     />
                 </tr>
             </table>
         </div>
-
     );
+}
+
+function moveShip(movementInstruction, shipIndex, shipData, setShipIndex) {
+    emptyOcean[shipIndex].occupant = ""
+    setShipIndex(shipIndex + movementInstruction)
+    emptyOcean[shipIndex + movementInstruction].occupant = shipData
+    
 }
 
 export default App;
